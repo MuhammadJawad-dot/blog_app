b809b8d43af51473d3d80f5436cd0f3ceca3a252
Muhammad Jawad <jawadintzar123@gmail.com>
2026-04-25 12:14:16 +0500

    post and category crud operations

---
+++ dashboard/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import get_object_or_404, render,redirect
+
+from blogs.models import Blog, Category
+from django.contrib.auth.decorators import login_required
+from .form import CategoryForm, PostForm
+from django.template.defaultfilters import slugify
+
+
+# Create your views here.
+@login_required(login_url='login')
+def dashboard(request):
+    category_count=Category.objects.all().count()
+    blogs_count=Blog.objects.all().count()
+    context={
+        'category_count': category_count,
+        'blogs_count': blogs_count
+    }
+    return render(request,'dashboard/dashboard.html',context)
+
+@login_required(login_url='login')
+def categories(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request,'dashboard/categories.html', context)
+
+def add_category(request):
+    if request.method == 'POST':
+        form=CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form=CategoryForm()
+    context={
+        'form':form
+    }
+    return render(request,'dashboard/add_category.html',context)
+
+def edit_category(request, pk):
+    category = Category.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category
+    }
+    return render(request, 'dashboard/edit_category.html', context)
+
+def delete_category(request, pk):
+    category = Category.objects.get(pk=pk)
+    category.delete()
+    return redirect('categories')
+
+def posts(request):
+    posts = Blog.objects.all()
+    context = {'posts': posts}
+    return render(request,'dashboard/post.html', context)
+
+
+
+def add_post(request):
+    if request.method=='POST':
+        form=PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.save()
+            title=form.cleaned_data['title']
+            post.slug=slugify(title)+'-'+str(post.id)
+            post.save()
+            return redirect('posts')
+    form=PostForm()
+    context={
+        'form':form
+    }
+    return render(request,'dashboard/add_post.html',context)
+
+def edit_post(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-'+str(post.id)
+            post.save()
+            return redirect('posts')
+    form = PostForm(instance=post)
+    context = {
+        'form': form,
+        'post': post
+    }
+    return render(request, 'dashboard/edit_post.html', context)
+
+def delete_post(request,pk):
+    post=get_object_or_404(Blog,pk=pk)
+    post.delete()
+    return redirect('posts')
