bff28e905d9369e131eaf953f97dcfd74d2ae3f2
Muhammad Jawad <jawadintzar123@gmail.com>
2026-04-29 18:35:11 +0500

    User Functionality

--- bolg_main/form.py
+++ bolg_main/form.py
@@ -5,3 +5,5 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+        
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render,redirect
 
 from blogs.models import Blog, Category
-from django.contrib.auth.decorators import login_required
-from .form import CategoryForm, PostForm
+from django.contrib.auth.decorators import login_required, permission_required
+from .form import CategoryForm, PostForm ,AddUserForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User 
 
 
 # Create your views here.
@@ -99,3 +100,46 @@ def delete_post(request,pk):
     post=get_object_or_404(Blog,pk=pk)
     post.delete()
     return redirect('posts')
+
+def user(request):
+    if not request.user.has_perm('auth.view_user'):
+        return render(request, '404.html')
+    user=User.objects.all()
+    context={
+        'user':user
+    }
+    return render(request,'dashboard/user.html',context)
+
+@permission_required('auth.add_user', login_url='login')
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user')
+    form = AddUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'dashboard/add_user.html', context)
+
+@permission_required('auth.change_user', login_url='login')
+def edit_user(request, pk):
+    user_obj = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = AddUserForm(request.POST, instance=user_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('user')
+    form = AddUserForm(instance=user_obj)
+    context = {
+        'form': form,
+        'user_obj': user_obj
+    }
+    return render(request, 'dashboard/edit_user.html', context)
+
+@permission_required('auth.delete_user', login_url='login')
+def delete_user(request, pk):
+    user_obj = get_object_or_404(User, pk=pk)
+    user_obj.delete()
+    return redirect('user')
